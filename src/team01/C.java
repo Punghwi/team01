918c84b4970ad4df6f93b564c0d141ae36806491
YD <fkrtn123@naver.com>
2022-03-16 14:32:09 +0900

    C작업 끝

--- src/team01/C.java
+++ src/team01/C.java
@@ -1,7 +1,7 @@
 package team01;
 
-public class B {
+public class C {
 	public static void main(String[] args) {
-		System.out.println("지훈이가 B작업 출력");
+		System.out.println("c작업 출력");
 	}
 }
--- src/team01/Common_team01.java
+++ src/team01/Common_team01.java
@@ -4,7 +4,9 @@ public class Common_team01 {
 
 	public static void main(String[] args) {
 		
+		System.out.println("A는 언제 작업끝????");
 		System.out.println("B작업끝");
+		System.out.println("C작업끝");
 		
 	}
 	
